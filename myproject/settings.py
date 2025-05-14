c06f2371021b9496d86b17a03f49ec79ab24d699
Onur Kapucu <kapucuonur@hotmail.com>
2025-05-14 16:15:20 +0300

    updates for media

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = MEDIA_DIR
+MEDIA_ROOT = '/opt/media'
 
 # ========================
 # Security Settings
