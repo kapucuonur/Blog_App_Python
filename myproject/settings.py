ad5dbeb2593881f3dbb6469be783091f179a3b98
kapucuonur <Gumushane1983.>
2026-04-15 14:19:39 +0300

    Add CSRF_TRUSTED_ORIGINS setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,6 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'https://blog.trihonor.com',
+]
 
 # ========================
 # Path Configurations
